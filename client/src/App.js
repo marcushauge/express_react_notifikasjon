a5ea3bbac14fc02026a57a14cbcd87b82fe33b09
marcushauge <marcus.hauge1@gmail.com>
2019-03-21 00:44:23 +0100

    In backend:  - Added one POST and one GET route for testing purposes.  - Added a POST route to receive notifications sent by client.
    In frontend:
     - Added input fields to create a notification entry.
     - Added one POST and one GET request method for testing purposes.
     - Added method notificationToBackend() to send a POST request with the notification to the backend.

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,8 @@ import './App.css';
 
 class App extends Component {
 state = {
-    data: null
+    data: null,
+    confirmation: null
   };
 
   componentDidMount() {
@@ -31,11 +32,73 @@ state = {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        // Render the newly fetched data inside of this.state.data 
+        Render the newly fetched data inside of this.state.data 
         <p className="App-intro">{this.state.data}</p>
+
+        <h2>Sende notifikasjon</h2>
+        Fra: <input type="text" id="frominput"></input>
+        Til: <input type="text" id="toinput"></input>
+        <input type="button" value="Send" onClick={() => this.notificationToBackend()}></input>
+        <p>{this.state.confirmation}</p>
+
       </div>
+
+      
     );
   }
+
+  //Function called by send button to make a POST request with the "notification" to the backend
+  notificationToBackend() {
+    //object with two properties to be sent in the request
+    var data = {"Sender": document.getElementById("frominput").value,
+                "Mottaker": document.getElementById("toinput").value
+    }
+
+    if(data.Sender === '' || data.Mottaker === '') {return}
+    
+    const options = {
+      method: "POST",
+      headers: {"Content-Type": "application/json"},
+      body:  JSON.stringify(data)
+    }
+
+    fetch('/send_notif', options)
+    .then(res => res.json())
+    .then(resJsonObj => console.log(resJsonObj))
+
+  }
+
+
+
+  //Function to test GET request
+  getReqTest() {
+    
+    fetch('/get_test')
+    .then(res => res.json())
+    .then(resJsonObj => console.log(resJsonObj))
+
+  }
+
+  //Function to test POST request
+  postReqTest() {
+    var data = {"navn": "Heisann"}
+    
+    //Optional object to pass in fetch() parameter
+    const options = {
+      method: "POST",
+      body:  JSON.stringify(data),
+      headers: new Headers({"Content-Type": "application/json"})
+    }
+
+    //make request and logs the response from the backend
+    fetch("/post_test", options)
+    .then(res => res.json())
+    .then(resJsonObj => console.log(resJsonObj))
+
+  }
+
+
+
 }
 
 export default App;
--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(express.json())
+app.use(express.urlencoded())
+var fs = require("fs");
+
 // console.log that your server is up and running
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
@@ -9,3 +13,40 @@ app.listen(port, () => console.log(`Listening on port ${port}`));
 app.get('/express_backend', (req, res) => {
   res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
 });
+
+
+//Create a POST route to handle incoming notification
+app.post('/send_notif', (req, res) => {
+
+  //Get the notification entry to be written to file
+  const sender = req.body.Sender
+  const mottaker = req.body.Mottaker
+  const linje = "Notifikasjon fra " + sender + " til " + mottaker + "\n"
+  
+  //Writes the line to the specified file
+  fs.appendFile('notifikasjoner.txt', linje, (err) => {  
+    // throws an error, you could also catch it here
+    if (err) throw err;
+
+    // success case, the file was saved
+    console.log('Notifikasjon skrevet til fil');
+
+});
+  //Send response back to client
+  res.send({responsemsg: "Notifikasjon mottatt"})
+
+})
+
+
+// GET route for testing
+app.get('/get_test', (req, res) => {
+  res.send({ testmsg: 'this is a get test message' });
+});
+
+// POST route for testing
+app.post('/post_test', (req, res) => {
+  console.log(req.body)
+  res.send({ testmsg: 'this is a post test message' });
+});
+
+
